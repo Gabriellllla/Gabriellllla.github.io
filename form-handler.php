64d4d0a1136bd520c12bfa987bb5339e128e8c40
Gabriellllla <99075840+Gabriellllla@users.noreply.github.com>
2023-05-10 11:12:45 +0300

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from='gabriellllla.github.io';
+$email_subject = 'Nou mesaj';
+$email_body="User Name: $name.\n".
+            "User Phone: $phone.\n".
+            "User Email: $email.\n".
+            "User Message: $message.\n";
+
+$to = 'marcugabriella02@gmail.com';
+
+$headers = "Form: &email_form \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+?>
